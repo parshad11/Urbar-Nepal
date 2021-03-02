a7725a6c3360e24bd50e8df838526dfd44475f66
sanjog karki <sanjogkarki1460>
2021-03-02 11:53:35 +0545

    user login api

--- app/User.php
+++ app/User.php
@@ -13,7 +13,7 @@ use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
     use SoftDeletes;
     use HasRoles;
     use HasApiTokens;
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+route::post('/login','api\LoginController@login')->name('delivery.login');
