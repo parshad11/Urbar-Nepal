a4edb70f0a96ccf15fad9cdb5a5ee4abcab9a4d8
PrdpPkrl25 <Pradip.Pokhrel25@gmail.com>
2021-03-17 10:40:56 +0545

    contact mail created

---
+++ resources/views/emails/vendor/request.blade.php
@@ -0,0 +1,15 @@
+@component('mail::message')
+Hello  {{ Config('mail.from.name') }}, 
+Someone has just contacted you from freshktm contact field with following detail.
+
+@component('mail::panel')
+Name: {{$name}}<br>
+Email: {{$email}}<br>
+Phone Number: {{$phone}}<br>
+Address: {{$address}}<br>
+Message: {{$message}}
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
