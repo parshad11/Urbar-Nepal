bcda024fbb09ff989553f2d51955eb492f0edd92
sanjog karki <sanjogkarki1460>
2021-03-22 10:35:15 +0545

    some changes

--- resources/views/sell/index.blade.php
+++ resources/views/sell/index.blade.php
@@ -122,9 +122,9 @@ $(document).ready( function(){
         serverSide: true,
         aaSorting: [[1, 'desc']],
         "ajax": {
-			 'headers': {
+			 /*'headers': {
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            },
+            },*/
             "url": "/sells",
             "data": function ( d ) {
                 if($('#sell_list_filter_date_range').val()) {
