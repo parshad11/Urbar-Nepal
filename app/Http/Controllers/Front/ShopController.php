4b45f9ed3a4f57dd74bf7e01cf09cca5a9670bf3
parshad11 <ghimire.parshad11@gmail.com>
2021-04-13 15:42:02 +0545

    admin icon

--- app/Http/Controllers/Front/ShopController.php
+++ app/Http/Controllers/Front/ShopController.php
@@ -137,16 +137,6 @@ class ShopController extends Controller
         $category_of_product = Category::with('sub_categories')->where('id',$category_id)->get();
         $sub_caategory_of_product = Category::with('sub_categories')->where('parent_id','!=',0)->get();
         return view('ecommerce.sub-catagories')->with(compact('sub_category_products','category_of_product','sub_caategory_of_product'));
-        // dd($product_category);
-        // if(count($sub_category_products)!=0){
-        //     $sub_category_products_ids = Product::where('sub_category_id', $id)->pluck('category_id')->toArray();
-        //     $category_of_product = Category::with('sub_categories')->where('id',$sub_category_products_ids)->first();
-        //     $sub_caategory_of_product = Category::with('sub_categories')->where('parent_id',$sub_category_products_ids)->get();
-        //     return view('ecommerce.sub-catagories')->with(compact('sub_category_products','category_of_product','sub_caategory_of_product'));
-        // }
-        // else{
-        //     return view('ecommerce.sub-catagories')->with(compact('sub_category_products'));
-        // }
 
                                                
     }
--- resources/views/ecommerce/partials/header.blade.php
+++ resources/views/ecommerce/partials/header.blade.php
@@ -219,14 +219,14 @@
 
                     </div>
 
-                    <div class="phone-header text-center pt-4">
+                    <!-- <div class="phone-header text-center pt-4">
                         <a href="#">
                             <span class="block">
                                 <i class="fas fa-phone-alt"></i> &nbsp;<span class="items" dir="ltr">+977
                                     9808666456</span>
                             </span>
                         </a>
-                    </div>
+                    </div> -->
                 </div>
             </div>
 
@@ -237,10 +237,10 @@
         <div class="container">
             <div class="row">
                 <div class="nav-category">
-                    <a href="allcategory.html" uk-toggle class="	">
+                    <a href="{{Route('show_all_category')}}" uk-toggle class="	">
                         <i class="fas fa-tasks mr-2"></i><span>Categories</span>
                     </a>
-                    <a href="allcategory.html" class="pl-3"> All category</a>
+                    <a href="" class="pl-3"> All category</a>
 
                 </div>
                 <ul class="nav-list-items d-flex">
